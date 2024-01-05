5ffdf7d6c0302857ff44ed1ca9c8aca423dc5e8d
fvd <fvd_24_93@hotmail.com>
2024-01-04 21:25:08 -0600

    Add dispatch action to update the state object

--- src/hooks/useTaskActions.jsx
+++ src/hooks/useTaskActions.jsx
@@ -8,11 +8,22 @@ const useTaskActions = () => {
     dispatch({ type: 'ADD_TASK', task: newTask });
   };
 
+  const handleUpdateCheckboxTask = (id) => {
+    dispatch({ type: 'UPDATE_CHECKBOX_TASK', id });
+  };
+
+  const handleUpdateButtonTask = (id) => {
+    dispatch({ type: 'UPDATE_BUTTON_TASK', id });
+  };
+  const handleUpdateTaskName = (id, updatedValue) => {
+    dispatch({ type: 'UPDATE_TASK_NAME', id , updatedValue});
+  };
+
   const handleDeleteTask = (id) => {
     dispatch({ type: 'DELETE_TASK', id });
   };
 
-  return { handleAddTask, handleDeleteTask, };
+  return { handleAddTask, handleDeleteTask, handleUpdateCheckboxTask, handleUpdateButtonTask, handleUpdateTaskName };
 };
 
 export default useTaskActions;
