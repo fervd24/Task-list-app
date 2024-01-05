4694d0afce991977a9ccad5163e812830d70e50d
fvd <fvd_24_93@hotmail.com>
2024-01-04 21:14:03 -0600

    refactor TaskForm

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,80 +1,31 @@
 import { useState } from "react";
-import TaskList from "./TaskList";
+import { TaskContext } from "../context/TaskContext";
+import useTaskActions from "../hooks/useTaskActions";
 
 
 function TaskForm() {
-    const [inputValue, setInputValue] = useState("");
-    const [taskList, setTaskList] = useState([])
+    const { dispatch } = useContext(TaskContext);
+    const [newTask, setNewTask] = useState('');
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value)
+    const taskActions = useTaskActions();
+
+    const handleAddTask = () => {
+        if (newTask.trim() !== '') {
+            taskActions.handleAddTask(newTask);
+            setNewTask('');
+        }
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        setTaskList([
-                ...taskList, 
-                {
-                    id: crypto.randomUUID(),
-                    task: inputValue,
-                    completed: false,
-                    isUpdating: false
-                }
-            ]);
-
-        setInputValue('');
-    }
-
-    
-    const handleUpdateTask = (id, propertyName, newValue) => {
-        setTaskList(taskList.map(task => {
-            if(task.id === id){
-                return {
-                    ...task,
-                    [propertyName]: newValue
-                };
-            }
-            return task;
-        }))
-    }
-
-    const handleDeleteTask = (id) => {
-        setTaskList(taskList.filter(task => task.id !== id))
-    }
-
-    const toggleCheckbox = (id) => {
-        setTaskList(taskList.map(task => {
-            if(task.id === id){
-                return {
-                    ...task,
-                    completed: !task.completed
-                };
-            }
-            return task;
-        }))
-    }
-
-    return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <input 
-                        type="text" 
-                        value={inputValue}
-                        onChange={handleInputChange}
-                    />
-                    <button type="submit">Add task</button>
-                </label>
-            </form>
-            <TaskList 
-                tasks={taskList} 
-                toggleCheckbox={toggleCheckbox} 
-                handleUpdateTask={handleUpdateTask}
-                handleDeleteTask={handleDeleteTask}
-            />
-        </>
-    )
+    return (
+        <div>
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button onClick={handleAddTask}>Add Task</button>
+        </div>
+      );
     
 }
 
