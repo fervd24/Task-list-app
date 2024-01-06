fb0837f9efc83c65d72d59734a39d954b8b7bb9e
fvd <fvd_24_93@hotmail.com>
2024-01-06 17:37:23 -0600

    Add some unit test

--- src/__test__/TaskApp.test.jsx
+++ src/__test__/TaskApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import TaskApp from "../components/TaskApp";
 
 describe("TaskApp", () => {
@@ -6,10 +6,32 @@ describe("TaskApp", () => {
     render(<TaskApp />);
     
   });
-
+  
   it("should find specific text in TaskApp", () => {
+    // Render the TaskForm component
     render(<TaskApp />);
+    // Assertion 
     expect(screen.findAllByText("Task List App"));
     
   });
+
+  it('renders task form correctly', () => {
+    const { getByPlaceholderText} = render(<TaskApp />);
+  
+    const addButton = screen.getByRole('button', { name: 'Add Task' });
+    const inputField = getByPlaceholderText('Enter a task...');
+  
+    expect(addButton).toBeInTheDocument();
+    expect(inputField).toBeInTheDocument();
+  });
+  
+  it("handle input to add new task correctly", () => {
+    const { getByPlaceholderText} = render(<TaskApp />);
+    const inputField = getByPlaceholderText('Enter a task...');
+    fireEvent.change(inputField, {target: {value: 'Test input'}});
+    
+    expect(inputField.value).toBe('Test input');
+  });
+  
+ 
 });
